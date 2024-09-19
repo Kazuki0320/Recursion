6ab1c282b998ebf2a7e19ee99b6175cd03eaa8f8
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-19 22:46:49 +0900

    BooleanInteger

---
+++ Beginner/BooleanInteger.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << true << endl;
+    cout << false << endl;
+
+    cout << 9999 << endl;
+
+    cout << 0b1111 << endl;
+    cout << 0b11 << endl;
+}
+
