de09980599d1c61801d85c271dae309291f42667
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-20 00:13:11 +0900

    Beginner:Char correct

---
+++ Beginner/Char/Char.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	cout << "The earth was bluish." << endl;
+	cout << "" << endl;
+
+	cout << 1+1 << endl;
+	cout << ("1" + std::string("1")) << endl;
+}
