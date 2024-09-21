44ededca5a200f939e3c9660eabfffa1e6d3d81a
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-21 13:30:17 +0900

    Recursion:Literal

---
+++ Beginner/Literal/Literal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void entry(){
+    cout << 'P' << endl;
+    cout << 2022 << endl;
+    cout << "I am learning primitive data. #RecursionCS" << endl;
+    cout << 12.3 << endl;
+    cout << false << endl;
+}
+
