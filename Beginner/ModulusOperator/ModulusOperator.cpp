7eb7a38df3953ed0fcda9016bfb688feb25f9ab9
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 11:44:07 +0900

    Recursion:ModulusOperator

---
+++ Beginner/ModulusOperator/ModulusOperator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath> 
+
+using namespace std;
+
+int main(){
+    if(87 % 2 == 1) {
+        cout << true << endl;
+    } else {
+        cout << false << endl;
+    }
+
+    int number1 = 3 * pow(10, 3);
+    if(number1 % 2 == 0) {
+        cout << true << endl;
+    } else {
+        cout << false << endl;
+    }
+}
+
