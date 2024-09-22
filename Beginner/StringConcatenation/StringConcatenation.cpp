f22373b9597590ba753dd0488e07b65df77d472c
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 16:36:55 +0900

    Recursion:StringConcatenation

---
+++ Beginner/StringConcatenation/StringConcatenation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string myFavoriteFood = "Sushi";
+    cout << "My Favorite Food : " + myFavoriteFood << endl;
+}
+
