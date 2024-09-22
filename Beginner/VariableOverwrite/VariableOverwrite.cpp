825c7fcc6f632b6402361676dabfae7ee5eeaed6
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 15:20:36 +0900

    Recursion:VariableOverwrite

---
+++ Beginner/VariableOverwrite/VariableOverwrite.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    bool isEmailVald = true;
+    cout << isEmailVald << endl;
+    isEmailVald = false;
+    cout << isEmailVald << endl;
+}
+
