4a386fb9182dd5273e3a7fe9188414a377fd7d21
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 11:16:04 +0900

    Recursion:Operator

---
+++ Beginner/Operator/Operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << 2 - 81 / 3 / 3 * 3 * 3 + 8 << endl;
+    cout << 3 + 10 / 2 * 5 << endl;
+    cout << 4 + 39 / (8 + 5) * 2 << endl;
+    cout << 5 + (80 + 1) / 9 / 3 * 5 << endl;
+}
+
