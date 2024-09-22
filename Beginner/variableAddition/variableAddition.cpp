1cbeb45124620622b60e2914e57c492d59c109ff
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 16:25:19 +0900

    Recursion:VariableAddition

---
+++ Beginner/variableAddition/variableAddition.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int onlinePaidUser = 40;
+    int onlineUser  = onlinePaidUser + 50;
+    cout << onlineUser << endl;
+}
