5926705aa4dc1487a3306a78eeb38f5d3bed3dd9
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 15:16:35 +0900

    Recursion:VariableDeclarationAndAssingment

---
+++ Beginner/VariableDeclarationAndAssignment/VariableDeclarationAndAssingment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string myFavoriteFruit = "Apple";
+    cout << myFavoriteFruit << endl;
+
+    int appleCount = 5;
+    cout << appleCount << endl;
+}
+
