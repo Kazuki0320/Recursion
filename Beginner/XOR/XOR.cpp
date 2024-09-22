7c86298f970f3751af5c85fd972184c07f34f257
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 23:10:20 +0900

    Recursion:XOR

---
+++ Beginner/XOR/XOR.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+bool myXOR(bool p, bool q){
+    // 関数を完成させてください
+    if(p == q) return false;
+    else return true;
+}
