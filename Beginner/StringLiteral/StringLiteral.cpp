175324891caf0f548e6457ee4b7a0e0e902698b5
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-22 11:58:39 +0900

    Recursion:StringLiteral

---
+++ Beginner/StringLiteral/StringLiteral.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Recursion"[0] << endl;
+    cout << "Recursion"[4] << endl;
+    cout << "Recursion"[8] << endl;
+}
+
